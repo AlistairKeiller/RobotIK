549639ace79350a871a0083c88c0acfe825be8fd
Alistair Keiller <alistair@keiller.net>
2023-03-19 22:36:05 -0700

    working on 2d arm fixes

--- 2darmik.jl
+++ 2darmik.jl
@@ -14,11 +14,11 @@ L2 = sg.sliders[2].value
 x = sg.sliders[3].value
 y = sg.sliders[4].value
 
-θ2 = @lift(acos((($x)^2+($y)^2-($L1)^2-($L2)^2)/(2*$L1*$L2)))
-θ1 = @lift(atan($x,$y)-atan($L1*sin($θ2)/($L1+$L2*cos($θ2))))
+θ2 = @lift(-acos((($x)^2+($y)^2-($L1)^2-($L2)^2)/(2*$L1*$L2)))
+θ1 = @lift(atan($y,$x)-atan($L1*sin($θ2),$L1+$L2*cos($θ2)))
 
-Axis(f[1,1], limits=(-4,4,-4,4))
+Axis(f[1,1], limits=(-4,4,-4,4), aspect = 1)
 
-lines!(@lift([Point2(0,0),Point2($L1*sin($θ1), $L1*cos($θ1)),Point2($L1*sin($θ1)+$L2*sin($θ1+$θ2), $L1*cos($θ1)+$L2*cos($θ1+$θ2))]))
+lines!(@lift([Point2(0,0),Point2($L1*cos($θ1), $L1*sin($θ1)),Point2($L1*cos($θ1)+$L2*cos($θ1+$θ2), $L1*sin($θ1)+$L2*sin($θ1+$θ2))]))
 
 scatter!(@lift([($x,$y)]))
