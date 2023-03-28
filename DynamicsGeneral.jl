ced5581553d82948a4520e45f8f38be7336a04b5
Alistair Keiller <Alistair Keiller>
2023-03-27 23:07:53 -0700

    add

--- DynamicsGeneral.jl
+++ DynamicsGeneral.jl
@@ -13,9 +13,15 @@ for i ∈ 2:n
     push!(P, P[i-1]+[L[i-1]*sin(sum(Symbolics.scalarize(θ[2:i]))),L[i-1]*cos(sum(Symbolics.scalarize(θ[2:i])))])
 end
 
-point_potential = sum([g*m[i]*P[i][2] for i in 1:n])
+total_point_potential = sum([g*m[i]*P[i][2] for i ∈ 1:n])
+total_line_potential = sum([g*m[i]*(P[i-1][2]+P[i][2])/2 for i ∈ 2:n])
+total_potential = point_potential + line_potential
 
-line_potential = sum([g*m[i]*(P[i-1][2]+P[i][2])/2 for i in 2:n])
-
-potential = point_potential + line_potential
-
+total_kenetic_energy = 0
+for J ∈ 1:n-1
+    point_inertia = sum(m[i]*((P[i][1]-P[J][1])^2+(P[i][2]-P[J][2])^2) for i ∈ J+1:n)
+    line_inertia = sum(M[i]/3*((P[i][1]-P[J][1])^2+(P[i][1]-P[J][1])*(P[i+1][1]-P[J][1])+(P[i+1][1]-P[J][1])^2+(P[i][2]-P[J][2])^2+(P[i][2]-P[J][2])*(P[i+1][2]-P[J][2])+(P[i+1][2]-P[J][2])^2) for i ∈ J:n-1)
+    inertia = point_inertia + line_inertia
+    kenetic_energy = 1/2*inertia*θ′[J+1]^2
+    total_kenetic_energy += kenetic_energy
+end
